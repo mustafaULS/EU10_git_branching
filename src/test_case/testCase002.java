c129c1082898d480ba6cbd96bee14941fc741bf5
mustafa <mtf.m114@gmail.com>
2022-12-09 19:05:47 +0300

    create test case 002

---
+++ src/test_case/testCase002.java
@@ -0,0 +1,11 @@
+package test_case;
+
+public class testCase002 {
+
+    public static void main(String[] args) {
+        System.out.println(" hello world");
+
+        System.out.println(" checking test case 002");
+
+    }
+}
